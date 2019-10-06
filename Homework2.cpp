8472f99059d9708fd36567d4e1787439c12a3494
matthew11j <jacks3mh@mail.uc.edu>
2019-10-06 16:14:41 -0400

    displayDocs

--- Homework2.cpp
+++ Homework2.cpp
@@ -6,12 +6,34 @@ using namespace std;
 class Sort
 {
 public:
+    /*
+      Display documentation describing program
+    */
     void displayDocs() {
-        cout << endl << "Player 1 (X) -- Player 2 (O)" << endl << endl;
-        cout << "    |     |" << endl;
-        cout << endl;
+        cout << "The purpose of this program is to allow for user testing" << endl;
+        cout << "of Mergesort and Quicksort algorithms where Insertionsort" << endl;
+        cout << "is used as a threshold sort." << endl << endl;
+        cout << "The program will begin by asking the user for inputs to be" << endl;
+        cout << "used in the program. These being:" << endl;
+        cout << "     Threshold Value" << endl << "     Size of sortable list" << endl;
+        cout << "     Whether the list should be created manually/automatically" << endl;
+        cout << "     Whether the list should be displayed or not" << endl << endl;
+        cout << "After valid values are provided, the program will either create" << endl;
+        cout << "an array of random numbers or the user will input list values depending" << endl;
+        cout << "on their inputs. The display of the initialized unsorted list will be displayed " << endl;
+        cout << "or not depending on these values as well." << endl << endl;
+        cout << "The implemented sorting algorithms will process the list and when complete," << endl;
+        cout << "display the sorted list." << endl;
+        cout << endl << endl << endl;
     }
 
+    /*
+      Reveives user inputs necessary for rest of program
+      i.e., Threshold value
+            Size of sortable list
+            Whether the list should be created manually/automatically
+            Whether the list should be displayed or not
+    */
     void getUserInput(int arr[]) {
         bool valid = false;
         bool largeList = false;
@@ -34,14 +56,14 @@ public:
 
         // Get user input for size of the list to sort
         do {
-            cout << "Please enter the size of the list to sort.\n" << endl;
+            cout << "Please enter the size of the list to sort." << endl;
             cin >> listSize;
             if (cin.good()) {
                 if (listSize > 1 && listSize < 16) {
                     arr[1] = listSize;
                     arr[2] = 0;
                     valid = true;
-                } else if (listSize > 15 && listSize < 40) {
+                } else if (listSize > 15 && listSize < 100) {
                     arr[1] = listSize;
                     arr[2] = 1;
                     valid = true;
@@ -63,7 +85,7 @@ public:
 
         // Get user input for initialization type
         while (!valid) {
-            cout << "Would you like the values in the list to be initialized Manually[1] or Automatically[2]?\n" << endl;
+            cout << "\nWould you like the values in the list to be initialized Manually[1] or Automatically[2]?" << endl;
             cin >> listCreation;
             if (cin.good()) {
                 if (listCreation == 1 || listCreation == 2) {
@@ -85,7 +107,7 @@ public:
 
         // Get user input for whether or not to display array
         while (!valid) {
-            cout << "Would you like to have the array displayed Yes[1] or No[0]?\n" << endl;
+            cout << "\nWould you like to have the array displayed Yes[1] or No[0]?" << endl;
             cin >> display;
             if (cin.good()) {
                 if (display == 1 || display == 0) {
@@ -102,13 +124,16 @@ public:
         }
     }
 
+    /*
+      Asks user to play again, so the app has ability to loop
+    */
     bool playAgain() {
         bool play = false;
         bool invalidChoice = true;
         char answer;
 
         while (invalidChoice == true) {
-            cout << "Would you like to play again? [y or n]?\n" << endl;
+            cout << "\nWould you like to play again? [y or n]?" << endl;
             cin >> answer;
 
             if (answer == 'y') {
@@ -123,6 +148,9 @@ public:
         return play;
     }
 
+    /*
+      Display array function
+    */
     void displayArray(int arr[], int size) {
         for (int i = 0; i < size; i++) {
             cout << "Value " << i << " is: " << arr[i] << endl;
@@ -133,6 +161,8 @@ public:
 int main() {
     int inputArray[5];
     int threshold, size, largeList, initializationType, display, random;
+    int mergeCnt = 0;
+    int quickCnt = 0;
     srand (time(NULL));
     bool play = true;
     bool valid = false;
