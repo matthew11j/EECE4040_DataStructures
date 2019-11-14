e42e3f281a7a7b9c0bb2a22943a83717cefd4e0d
matthew11j <jacks3mh@mail.uc.edu>
2019-11-14 16:00:56 -0500

    WIP

--- Homework4.cpp
+++ Homework4.cpp
@@ -1,96 +1,121 @@
 #include <iostream>
 #include <list>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
-class Graph 
-{ 
-    int V;    
-    list<int> *adj; 
-  
-public: 
-    Graph(int V) 
-    { 
-        this->V = V; 
-        adj = new list<int>[V]; 
-    } 
-  
-    // function to add an edge to graph 
-    void addEdge(int u, int v) 
-    { 
-        adj[u].push_back(v); 
-    } 
-    // prints a Topological Sort of the complete graph 
-    void topologicalSort() 
-    { 
-        // Create a vector to store indegrees of all 
-        // vertices. Initialize all indegrees as 0. 
-        vector<int> in_degree(V, 0); 
-    
-        // Traverse adjacency lists to fill indegrees of 
-        // vertices.  This step takes O(V+E) time 
-        for (int u=0; u<V; u++) 
-        { 
-            list<int>::iterator itr; 
-            for (itr = adj[u].begin(); itr != adj[u].end(); itr++) 
-                in_degree[*itr]++; 
-        } 
-    
-        // Create an queue and enqueue all vertices with 
-        // indegree 0 
-        queue<int> q; 
-        for (int i = 0; i < V; i++) 
-            if (in_degree[i] == 0) 
-                q.push(i); 
-    
-        // Initialize count of visited vertices 
-        int cnt = 0; 
-    
-        // Create a vector to store result (A topological 
-        // ordering of the vertices) 
-        vector <int> top_order; 
-    
-        // One by one dequeue vertices from queue and enqueue 
-        // adjacents if indegree of adjacent becomes 0 
-        while (!q.empty()) 
-        { 
-            // Extract front of queue (or perform dequeue) 
-            // and add it to topological order 
-            int u = q.front(); 
-            q.pop(); 
-            top_order.push_back(u); 
-    
-            // Iterate through all its neighbouring nodes 
-            // of dequeued node u and decrease their in-degree 
-            // by 1 
-            list<int>::iterator itr; 
-            for (itr = adj[u].begin(); itr != adj[u].end(); itr++) 
-    
-                // If in-degree becomes zero, add it to queue 
-                if (--in_degree[*itr] == 0) 
-                    q.push(*itr); 
-    
-            cnt++; 
-        } 
-    
-        // Check if there was a cycle 
-        if (cnt != V) 
-        { 
-            cout << "There exists a cycle in the graph\n"; 
-            return; 
-        } 
-    
-        // Print topological order 
-        for (int i=0; i<top_order.size(); i++) 
-            cout << top_order[i] << " "; 
-        cout << endl; 
-    } 
-}; 
-  
-int main() 
-{ 
-    
-  
-    return 0; 
-} 
+struct node
+  {
+    int data;
+    node *next;
+  };
+
+class Digraph
+{
+    int V;
+    list<int> *adj;
+
+public:
+    Digraph(int V)
+    {
+        this->V = V;
+        adj = new list<int>[V];
+    }
+
+    // function to add an edge to graph
+    void addEdge(int u, int v)
+    {
+      //WIP
+        //adj[u].push_back(v);
+        node *head = adj[u];
+
+        node *temp = new node;
+        temp->data = v;
+        temp->next = NULL;
+        if (head == NULL)
+        {
+          head = temp;
+          tail = temp;
+          temp = NULL:
+        }
+        else
+        {
+          tail->next = temp;
+          tail = temp;
+        }
+    }
+
+    void topologicalSortUtil(int v, bool visited[], stack<int> &Stack)
+    {
+        // Mark the current node as visited.
+        visited[v] = true;
+
+        // Recur for all the vertices adjacent to this vertex
+        list<int>::iterator i;
+        for (i = adj[v].begin(); i != adj[v].end(); ++i)
+            if (!visited[*i])
+                topologicalSortUtil(*i, visited, Stack);
+
+        // Push current vertex to stack which stores result
+        Stack.push(v);
+    }
+
+    // prints a Topological Sort of the complete graph
+    void topologicalSort()
+    {
+        stack<int> Stack;
+
+        // Mark all the vertices as not visited
+        bool *visited = new bool[V];
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+
+        // Call the recursive helper function to store Topological
+        // Sort starting from all vertices one by one
+        for (int i = 0; i < V; i++)
+          if (visited[i] == false)
+            topologicalSortUtil(i, visited, Stack);
+
+        // Print contents of stack
+        while (Stack.empty() == false)
+        {
+            cout << Stack.top() << " ";
+            Stack.pop();
+        }
+    }
+
+    // Displays Digraph
+    void displayDigraph()
+    {
+        for (int u = 0; u < V; ++u)
+        {
+            cout << "\n Vertex " << u << ": head";
+            for (int v : adj[u])
+               cout << " -> " << v;
+        }
+        printf("\n");
+    }
+
+    void acyclicCheck()
+    {
+      // TODO
+    }
+};
+
+int main()
+{
+    // Create a graph given in the above diagram
+    Digraph dg(6);
+    dg.addEdge(5, 2);
+    dg.addEdge(5, 0);
+    dg.addEdge(4, 0);
+    dg.addEdge(4, 1);
+    dg.addEdge(2, 3);
+    dg.addEdge(3, 1);
+    dg.displayDigraph();
+
+    //cout << "Following is a Topological Sort of the given graph \n";
+    //dg.topologicalSort();
+
+    return 0;
+}
