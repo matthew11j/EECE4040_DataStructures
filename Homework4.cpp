498c321e2f0f9b168288f1a105a3b9bca83f32ec
Nathan Sucher <suchernathan@gmail.com>
2019-11-14 13:08:38 -0500

    started

---
+++ Homework4.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+class Graph 
+{ 
+    int V;    
+    list<int> *adj; 
+  
+public: 
+    Graph(int V) 
+    { 
+        this->V = V; 
+        adj = new list<int>[V]; 
+    } 
+  
+    // function to add an edge to graph 
+    void addEdge(int u, int v) 
+    { 
+        adj[u].push_back(v); 
+    } 
+    // prints a Topological Sort of the complete graph 
+    void topologicalSort() 
+    { 
+        // Create a vector to store indegrees of all 
+        // vertices. Initialize all indegrees as 0. 
+        vector<int> in_degree(V, 0); 
+    
+        // Traverse adjacency lists to fill indegrees of 
+        // vertices.  This step takes O(V+E) time 
+        for (int u=0; u<V; u++) 
+        { 
+            list<int>::iterator itr; 
+            for (itr = adj[u].begin(); itr != adj[u].end(); itr++) 
+                in_degree[*itr]++; 
+        } 
+    
+        // Create an queue and enqueue all vertices with 
+        // indegree 0 
+        queue<int> q; 
+        for (int i = 0; i < V; i++) 
+            if (in_degree[i] == 0) 
+                q.push(i); 
+    
+        // Initialize count of visited vertices 
+        int cnt = 0; 
+    
+        // Create a vector to store result (A topological 
+        // ordering of the vertices) 
+        vector <int> top_order; 
+    
+        // One by one dequeue vertices from queue and enqueue 
+        // adjacents if indegree of adjacent becomes 0 
+        while (!q.empty()) 
+        { 
+            // Extract front of queue (or perform dequeue) 
+            // and add it to topological order 
+            int u = q.front(); 
+            q.pop(); 
+            top_order.push_back(u); 
+    
+            // Iterate through all its neighbouring nodes 
+            // of dequeued node u and decrease their in-degree 
+            // by 1 
+            list<int>::iterator itr; 
+            for (itr = adj[u].begin(); itr != adj[u].end(); itr++) 
+    
+                // If in-degree becomes zero, add it to queue 
+                if (--in_degree[*itr] == 0) 
+                    q.push(*itr); 
+    
+            cnt++; 
+        } 
+    
+        // Check if there was a cycle 
+        if (cnt != V) 
+        { 
+            cout << "There exists a cycle in the graph\n"; 
+            return; 
+        } 
+    
+        // Print topological order 
+        for (int i=0; i<top_order.size(); i++) 
+            cout << top_order[i] << " "; 
+        cout << endl; 
+    } 
+}; 
+  
+int main() 
+{ 
+    
+  
+    return 0; 
+} 
